bf6d2ef3fae10eefd89e565a8de6050a0775907a
Juan <jaguirreb@fi.uba.ar>
2023-04-05 20:48:57 -0300

    Can create users, missing integration with firebase TODO: review models

--- cmd/gateway_test.go
+++ cmd/gateway_test.go
@@ -1,16 +1,19 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+
+	"fiufit.api.gateway/internal/auth"
 	"net/http"
 	"net/http/httptest"
 	"net/url"
-
 	"testing"
 
 	"github.com/gin-gonic/gin"
 )
 
-func TestUserSignUp(t *testing.T) {
+func TestGateway(t *testing.T) {
 	gin.SetMode(gin.TestMode)
 	assertStatusCode := func(t testing.TB, got, want int) {
 		t.Helper()
@@ -34,36 +37,56 @@ func TestUserSignUp(t *testing.T) {
 		defer server.Close()
 
 		url, _ := url.Parse(server.URL)
-		gateway := NewGateway(func(p Proxy, r *gin.Engine) {
-			r.POST("/test", p(url))
+		gateway := NewGateway(func(r *gin.Engine) {
+			r.POST("/test", reverseProxy(url))
 		})
 
 		w := CreateTestResponseRecorder()
 		req, _ := http.NewRequest("POST", "/test", nil)
-
 		gateway.ServeHTTP(w, req)
 
 		assertStatusCode(t, w.Code, http.StatusOK)
 		assertBody(t, w.Body.String(), "reverse-proxy")
 	})
 
-	// t.Run("Receive a request to sign up a new user, authorize it and send it to user service",
-	// 	func(t *testing.T) {
-	// 		testURL, _ := url.Parse("http://localhost:8080")
-	// 		gateway := NewGateway(testURL)
+	t.Run("Receive a request to sign up a new user, authorize it and notify both client and Users service",
+		func(t *testing.T) {
+			usersService := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(http.StatusCreated)
+				w.Header().Set("Content-Type", "application/text")
+			}))
+			defer usersService.Close()
+			usersServiceURL, _ := url.Parse(usersService.URL)
+			s := AuthTestService{}
+			gateway := NewGateway(func(r *gin.Engine) {
+				r.POST("/users", createUser(usersServiceURL, s))
+			})
 
-	// 		w := httptest.NewRecorder()
+			signUpData := auth.SignUpModel{
+				Email: "abc@xyz.com", Username: "abc", Password: "123",
+			}
+			signUpDataJSON, _ := json.Marshal(signUpData)
+			w := CreateTestResponseRecorder()
+			req, _ := http.NewRequest("POST", "/users", bytes.NewReader(signUpDataJSON))
+			gateway.ServeHTTP(w, req)
 
-	// 		body, _ := json.Marshal(SignUpData{"user@abc.com", "1234"})
-	// 		req, _ := http.NewRequest("POST", "/users", bytes.NewReader(body))
-	// 		gateway.ServeHTTP(w, req)
-	// 		assertStatusCode(t, w.Code, http.StatusOK)
+			assertStatusCode(t, w.Code, http.StatusCreated)
 
-	// 		expectedJsonResponse, _ := json.Marshal(AuthData{"123", "abc", "xyz"})
-	// 		assertBody(t, w.Body.String(), string(expectedJsonResponse))
-	// 	})
+			authData := auth.AuthorizedModel{UID: "123", Token: "abc", RefreshToken: "xyz"}
+			authDataJSON, _ := json.Marshal(authData)
+			assertBody(t, w.Body.String(), string(authDataJSON))
+		})
 }
 
+type AuthTestService struct{}
+
+func (a AuthTestService) CreateUser(s auth.SignUpModel) (auth.AuthorizedModel, error) {
+	return auth.AuthorizedModel{UID: "123", Token: "abc", RefreshToken: "xyz"}, nil
+}
+
+// The types below are necessary for tests to run Gin requires that
+// the recorder implements the CloseNotify interface. So we generated
+// a wrapper that implements it.
 type TestResponseRecorder struct {
 	*httptest.ResponseRecorder
 	closeChannel chan bool
--- cmd/main.go
+++ cmd/main.go
@@ -1,23 +1,18 @@
 package main
 
 import (
-	"net/http"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
-type SignUpData struct {
+type UserModel struct {
+	UID      string `json:"uid"`
 	Email    string `json:"email"`
-	Password string `json:"password"`
-}
-
-type AuthData struct {
-	UID string `json:"uid"`
-	Token string `json:"token"`
-	RefreshToken string `json:"refresh_token"`
+	Username string `json:"username"`
 }
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {c.JSON(http.StatusOK, "pong!")})
+	r.GET("/ping", func(c *gin.Context) { c.JSON(http.StatusOK, "pong!") })
 	r.Run() // listen and serve on 0.0.0.0:8080
 }
