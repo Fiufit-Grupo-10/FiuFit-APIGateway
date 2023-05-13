ade1190d10ef73488d90b23e1fa07b71bbfdb77f
Juan <jaguirreb@fi.uba.ar>
2023-05-12 22:05:48 -0300

    Added review support

--- cmd/gateway/handlers.go
+++ cmd/gateway/handlers.go
@@ -87,9 +87,36 @@ func GetTrainerPlans(url *url.URL, s auth.Service) gin.HandlerFunc {
 	trainersServiceURL := &*url
 	return func(ctx *gin.Context) {
 		middleware.AuthorizeUser(s)(ctx)
-		// Verify that the user is indeed a trainer
+		// Verify that the user is indeed a trainer, and that it's the same
 		middleware.AbortIfNotAuthorized(ctx)
-		middleware.AddUIDToRequestURL()(ctx)
+		middleware.ReverseProxy(trainersServiceURL)(ctx)
+	}
+}
+
+func GetAllTrainersPlans(url *url.URL, s auth.Service) gin.HandlerFunc {
+	trainersServiceURL := &*url
+	return func(ctx *gin.Context) {
+		middleware.AuthorizeUser(s)(ctx)
+		middleware.AbortIfNotAuthorized(ctx)
+		middleware.ReverseProxy(trainersServiceURL)(ctx)		
+	}
+}
+
+func CreateReview(url *url.URL, s auth.Service) gin.HandlerFunc {
+	trainersServiceURL := &*url
+	return func(ctx *gin.Context) {
+		middleware.AuthorizeUser(s)(ctx)
+		middleware.AbortIfNotAuthorized(ctx)
+		middleware.ReverseProxy(trainersServiceURL)(ctx)
+	}
+}
+
+func GetTrainingPlanReviews(url *url.URL, s auth.Service) gin.HandlerFunc {
+	trainersServiceURL := &*url
+	return func(ctx *gin.Context) {
+		middleware.AuthorizeUser(s)(ctx)
+		// Verify that the user is indeed a trainer, and that it's the same
+		middleware.AbortIfNotAuthorized(ctx)
 		middleware.ReverseProxy(trainersServiceURL)(ctx)
 	}
 }
--- cmd/main.go
+++ cmd/main.go
@@ -36,8 +36,8 @@ func main() {
 	if err != nil {
 		log.Fatalf("Error parsing URL: %s", trainersURL)
 	}
-	
-	gateway := gateway.New(gateway.Users(usersURL, f), gateway.Admin(usersURL, f), gateway.Trainers(trainersURL, f))
-	
+
+	gateway := gateway.New(gateway.Users(usersURL, f), gateway.Admin(usersURL, f), gateway.Trainers(trainersURL, f), gateway.Reviews(trainersURL, f))
+
 	gateway.Run("0.0.0.0:8080")
 }
