679d43f1ce70c736f9af9490a785addd22485cbc
Juan <jaguirreb@fi.uba.ar>
2023-05-14 14:21:23 -0300

    Modified handlers

--- cmd/gateway/handlers.go
+++ cmd/gateway/handlers.go
@@ -8,62 +8,6 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func CreateUser(url *url.URL, s auth.Service) gin.HandlerFunc {
-	usersServiceURL := &*url
-	return func(ctx *gin.Context) {
-		middleware.CreateUser(s)(ctx)
-		middleware.ReverseProxy(usersServiceURL)(ctx)
-	}
-}
-
-func GetUsersProfiles(url *url.URL, s auth.Service) gin.HandlerFunc {
-	usersServiceURL := &*url
-	return func(ctx *gin.Context) {
-		middleware.AuthorizeUser(s)(ctx)
-		middleware.ExecuteIf(middleware.IsAuthorized, middleware.AddUIDToRequestURL(), middleware.SetQuery("admin", "false"))(ctx)
-		middleware.ReverseProxy(usersServiceURL)(ctx)
-	}
-}
-
-func UpdateUserProfile(url *url.URL, s auth.Service) gin.HandlerFunc {
-	usersServiceURL := &*url
-	return func(ctx *gin.Context) {
-		middleware.AuthorizeUser(s)(ctx)
-		// Verify that it's the same user
-		middleware.AbortIfNotAuthorized(ctx)
-		middleware.ReverseProxy(usersServiceURL)(ctx)
-	}
-}
-
-func CreateAdmin(url *url.URL, s auth.Service) gin.HandlerFunc {
-	usersServiceURL := &*url
-	return func(ctx *gin.Context) {
-		middleware.AuthorizeUser(s)(ctx)
-		middleware.AuthorizeAdmin(usersServiceURL)(ctx)
-		middleware.CreateAdmin(s)(ctx)
-		middleware.ReverseProxy(usersServiceURL)(ctx)
-	}
-}
-
-func GetUsersProfilesAdmin(url *url.URL, s auth.Service) gin.HandlerFunc {
-	usersServiceURL := &*url
-	return func(ctx *gin.Context) {
-		middleware.AuthorizeUser(s)(ctx)
-		middleware.AbortIfNotAuthorized(ctx)
-		middleware.AuthorizeAdmin(usersServiceURL)(ctx)
-		middleware.RemovePathFromRequestURL("/admins")(ctx)
-		middleware.SetQuery("admin", "true")(ctx)
-		middleware.ReverseProxy(usersServiceURL)(ctx)
-	}
-}
-
-func GetTrainingTypes(url *url.URL) gin.HandlerFunc {
-	usersServiceURL := &*url
-	return func(ctx *gin.Context) {
-		middleware.ReverseProxy(usersServiceURL)(ctx)
-	}
-}
-
 func CreateTrainingPlan(url *url.URL, s auth.Service) gin.HandlerFunc {
 	trainersServiceURL := &*url
 	return func(ctx *gin.Context) {
