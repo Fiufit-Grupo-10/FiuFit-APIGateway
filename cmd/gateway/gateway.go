481a89549e4a07c21fc7b5a156fe1b295a61a3c9
Juan <jaguirreb@fi.uba.ar>
2023-06-10 12:28:53 -0300

    get panel

--- cmd/gateway/gateway.go
+++ cmd/gateway/gateway.go
@@ -202,6 +202,11 @@ func Goals(url *url.URL, s auth.Service) RouterConfig {
 			middleware.AuthorizeUser(s),
 			middleware.AbortIfNotAuthorized,
 			middleware.ReverseProxy(*&url))
+
+		router.GET("/users/:user_id/training",
+			middleware.AuthorizeUser(s),
+			middleware.AbortIfNotAuthorized,
+			middleware.ReverseProxy(*&url))
 	}
 }
 
